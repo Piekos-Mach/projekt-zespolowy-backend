822bc6f8e5a908961c6d7a393064849d60956247
blazej <blazejp.bp@gmail.com>
2020-11-03 22:05:34 +0100

    Initialization of project.

---
+++ src/main/java/com/projekt_zespolowy/tablica_ogloszen/TablicaOgloszenApplication.java
@@ -0,0 +1,13 @@
+package com.projekt_zespolowy.tablica_ogloszen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TablicaOgloszenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TablicaOgloszenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projekt_zespolowy/tablica_ogloszen/TablicaOgloszenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekt_zespolowy.tablica_ogloszen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TablicaOgloszenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
