66b91109fcef34a6609c489bb027ab505e47d42f
Blazej <Blazej@DESKTOP-P8SE49K>
2020-11-22 20:50:18 +0200

    Validation sequence has been implemented.

---
+++ src/main/java/com/projekt_zespolowy/tablica_ogloszen/validation/DefaultSequence.java
@@ -0,0 +1,8 @@
+package com.projekt_zespolowy.tablica_ogloszen.validation;
+
+import javax.validation.GroupSequence;
+
+@GroupSequence({FirstLevel.class, SecondLevel.class})
+public interface DefaultSequence {
+}
+
---
+++ src/main/java/com/projekt_zespolowy/tablica_ogloszen/validation/SecondLevel.java
@@ -0,0 +1,4 @@
+package com.projekt_zespolowy.tablica_ogloszen.validation;
+
+public class SecondLevel {
+}
