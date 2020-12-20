1e2fb3e2010aa39ebbded69e8b306d65aa7e7b87
blazej <blazejp.bp@gmail.com>
2020-12-20 16:04:57 +0100

    Missing constructor in OfferPageView.class fixed.

--- src/main/java/com/projekt_zespolowy/tablica_ogloszen/models/offer/OfferPageView.java
+++ src/main/java/com/projekt_zespolowy/tablica_ogloszen/models/offer/OfferPageView.java
@@ -13,12 +13,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class OfferPageView {
 
-    private Long id;
-    private BasicView owner = new BasicView();
-    private String title;
-    private String text;
-    private PriceView price = new PriceView();
-    private ImageView image = new ImageView();
-    private LocalDateTime creationDate;
+  private Long id;
+  private BasicView owner = new BasicView();
+  private String title;
+  private String text;
+  private PriceView price = new PriceView();
+  private ImageView image = new ImageView();
+  private LocalDateTime creationDate;
+
+  public OfferPageView(
+      Long id,
+      BasicView owner,
+      String title,
+      String text,
+      PriceView price,
+      LocalDateTime creationDate) {
+
+    this.id = id;
+    this.owner = owner;
+    this.title = title;
+    this.text = text;
+    this.price = price;
+    this.creationDate = creationDate;
+  }
 
 }
